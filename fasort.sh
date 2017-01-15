d33682493523df1ddef888d390b14dfbead47b65
noxy <loonwings@gmail.com>
2017-01-15 15:33:46 -0600

    First working example

--- fasort.sh
+++ fasort.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#set -xv
 
-# List files
-ls | egrep '$\d+\.[0-9a-zA-Z-]+_.+\.\w+^' > fasort.txt 
+SORTDIR=fasorted
+
+for f in `ls | egrep '^\d+\.[0-9a-zA-Z-]+_.+\.\w+$'`
+do
+    ARTIST=$(echo $f | awk -F '.' '{print$2}' | awk -F _ '{print$1}')
+    if [ ! -d $SORTDIR/$ARTIST ]; then
+        mkdir -p $SORTDIR/$ARTIST
+    fi
+    mv $f $SORTDIR/$ARTIST/$f
+done
